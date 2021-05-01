e28104fbd2db67e1bc911e6705cd685360bcb3dd
abdou30.cia <abdou30.cia@gmail.com>
2021-05-01 14:25:57 +0000

    fixing filter robots

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,8 +27,9 @@ export default class App extends Component {
 
         const { robots, searchfield } = this.state;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLocaleUpperCase())
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
+        console.log(filteredRobots);
 
         return !robots.length ? <h1>Loading</h1> : (
             <div className='tc'>
